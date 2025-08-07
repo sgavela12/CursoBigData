b4c71cd27c279780202024b868b8461ca06afd4c
Sergio Gavela Jiménez <sergio.gavela@bosonit.com>
2025-08-07 14:29:39 +0200

    cambios en la estructura de main y ejercicios

--- EjerciciosDataFrameAvanzados/src/main/scala/EjerciciosDataFrame.scala
+++ EjerciciosDataFrameAvanzados/src/main/scala/EjerciciosDataFrame.scala
@@ -1,138 +1,125 @@
-import org.apache.spark.sql.{SparkSession, DataFrame}
-import org.apache.spark.sql.functions._
+  import org.apache.spark.sql.{SparkSession, DataFrame}
+  import org.apache.spark.sql.functions._
 
-object EjerciciosDataFrame {
+  object EjerciciosDataFrame {
 
-  // rutas
-  val userCSV = "data/raw/googleplaystore_user_review.csv"
-  val playStoreCSV = "data/raw/googleplaystore.csv"
+    
 
-  val spark = SparkSession
-    .builder()
-    .appName("TransformarCSV")
-    .master("local[*]")
-    .getOrCreate()
+    // ------EJERCICIOS------
 
-  import spark.implicits._
+    // EJERCICIO 1
 
-  val dfUsers = spark.read.option("header", "true").csv(userCSV)
-  val dfPlayStore = spark.read.option("header", "true").csv(playStoreCSV)
+    def ejercicio1(dfPlayStore : DataFrame): Unit = {
 
-  // ------EJERCICIOS------
+      val dfFiltrado =
+        dfPlayStore.filter(col("App") =!= "Life Made WI-FI Touchscreen Photo Frame")
 
-  // EJERCICIO 1
+      dfFiltrado.show()
 
-  def ejercicio1(): Unit = {
+    }
 
-    val dfFiltrado =
-      dfPlayStore.filter($"App" =!= "Life Made WI-FI Touchscreen Photo Frame")
+    // EJERCICIO 2
 
-    dfFiltrado.show()
+    def ejercicio2(dfPlayStore : DataFrame): Unit = {
 
-  }
+      // Rellenar valores nulos o NaN en Rating con 0.0
+      val dfConCeros = dfPlayStore.na.fill(Map("Rating" -> 0.0))
 
-  // EJERCICIO 2
+      dfConCeros.show()
 
-  def ejercicio2(): Unit = {
+    }
 
-    // Rellenar valores nulos o NaN en Rating con 0.0
-    val dfConCeros = dfPlayStore.na.fill(Map("Rating" -> 0.0))
+    // EJERCICIO 3
 
-    dfConCeros.show()
+    def ejercicio3(dfPlayStore : DataFrame): Unit = {
 
-  }
+      val dfUnknown = dfPlayStore.na.fill(Map("Type" -> "Unknown"))
 
-  // EJERCICIO 3
+      dfUnknown.show()
 
-  def ejercicio3(): Unit = {
+    }
 
-    val dfUnknown = dfPlayStore.na.fill(Map("Type" -> "Unknown"))
+    // EJERCICIO 4
 
-    dfUnknown.show()
+    def ejercicio4(dfPlayStore : DataFrame): Unit = {
 
-  }
-
-  // EJERCICIO 4
-
-  def ejercicio4(): Unit = {
-
-    val df2 = dfPlayStore.withColumn(
-      "Varia",
-      when(col("Android Ver") === ("Varies with device"), true).otherwise(false)
-    )
-    df2.show()
-
-  }
-
-  // EJERCICIO 5
-  def ejercicio5(): Unit = {
-
-    val dfFrec_DowLoads = dfPlayStore.withColumn(
-      "Frec_DownLoads",
-      when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
-        .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
-        .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
-        .when(col("Installs") >= 50000000, "Muy alta")
-        .otherwise("Algo ha ido mal")
-    )
-    dfFrec_DowLoads.show()
-
-  }
-
-  // EJERCICIO 6A
-  def ejercicio6a(): Unit = {
-
-    val dfFrec_DowLoads = dfPlayStore.withColumn(
-      "Frec_DownLoads",
-      when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
-        .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
-        .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
-        .when(col("Installs") >= 50000000, "Muy alta")
-        .otherwise("Algo ha ido mal")
-    )
-    val bestApp = dfFrec_DowLoads
-      .filter(
-        col("Frec_DownLoads") === "Muy alta"
-          && col("Rating") >= 4.5
+      val df2 = dfPlayStore.withColumn(
+        "Varia",
+        when(col("Android Ver") === ("Varies with device"), true).otherwise(false)
       )
-      .select("App", "Rating", "Frec_DownLoads")
+      df2.show()
 
-    bestApp.show()
+    }
 
-  }
+    // EJERCICIO 5
+    def ejercicio5(dfPlayStore : DataFrame): Unit = {
 
-  // EJERCICIO 6B
-  def ejercicio6b(): Unit = {
-
-    val dfFrec_DowLoads = dfPlayStore.withColumn(
-      "Frec_DownLoads",
-      when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
-        .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
-        .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
-        .when(col("Installs") >= 50000000, "Muy alta")
-        .otherwise("Algo ha ido mal")
-    )
-    val bestFreeApp = dfFrec_DowLoads
-      .filter(
-        col("Frec_DownLoads") === "Muy alta"
-          && col("Price") == 0
+      val dfFrec_DowLoads = dfPlayStore.withColumn(
+        "Frec_DownLoads",
+        when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
+          .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
+          .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
+          .when(col("Installs") >= 50000000, "Muy alta")
+          .otherwise("Algo ha ido mal")
       )
-      .select("App", "Price", "Frec_DownLoads")
+      dfFrec_DowLoads.show()
 
-    bestFreeApp.show()
+    }
 
+    // EJERCICIO 6A
+    def ejercicio6a(dfPlayStore : DataFrame): Unit = {
+
+      val dfFrec_DowLoads = dfPlayStore.withColumn(
+        "Frec_DownLoads",
+        when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
+          .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
+          .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
+          .when(col("Installs") >= 50000000, "Muy alta")
+          .otherwise("Algo ha ido mal")
+      )
+      val bestApp = dfFrec_DowLoads
+        .filter(
+          col("Frec_DownLoads") === "Muy alta"
+            && col("Rating") >= 4.5
+        )
+        .select("App", "Rating", "Frec_DownLoads")
+
+      bestApp.show()
+
+    }
+
+    // EJERCICIO 6B
+    def ejercicio6b(dfPlayStore : DataFrame): Unit = {
+
+      val dfFrec_DowLoads = dfPlayStore.withColumn(
+        "Frec_DownLoads",
+        when(col("Installs") >= 0 && col("Installs") < 50000, "Baja")
+          .when(col("Installs") >= 50000 && col("Installs") < 1000000, "Media")
+          .when(col("Installs") >= 1000000 && col("Installs") < 50000000, "Alta")
+          .when(col("Installs") >= 50000000, "Muy alta")
+          .otherwise("Algo ha ido mal")
+      )
+      val bestFreeApp = dfFrec_DowLoads
+        .filter(
+          col("Frec_DownLoads") === "Muy alta"
+            && col("Price") == 0
+        )
+        .select("App", "Price", "Frec_DownLoads")
+
+      bestFreeApp.show()
+
+    }
+
+    // EJERCICIO 6C
+
+    def ejercicio6c(dfPlayStore : DataFrame): Unit = {
+
+      dfPlayStore.filter(col("Price") < 13).show()
+
+    }
+    // EJERCICIO 7
+    def ejercicio7(dfPlayStore : DataFrame): Unit = {
+      val dfSample = dfPlayStore.sample(withReplacement = false, fraction = 0.1, seed = 123)
+  dfSample.show()
+    }
   }
-
-  // EJERCICIO 6C
-
-  def ejercicio6c(): Unit = {
-
-    dfPlayStore.filter(col("Price") < 13).show()
-
-  }
-  // EJERCICIO 7
-  def ejercicio7(): Unit = {
-    val dfSample = dfPlayStore.sample(withReplacement = false, fraction = 0.1, seed = 123)
-dfSample.show()
-  }
-}
--- EjerciciosDataFrameAvanzados/src/main/scala/Main.scala
+++ EjerciciosDataFrameAvanzados/src/main/scala/Main.scala
@@ -1,6 +1,40 @@
-object Main extends App {
-  
+import org.apache.spark.sql.SparkSession
 
-  EjerciciosDataFrame.ejercicio7()
+object Main {
+  def main(args: Array[String]): Unit = {
 
+
+
+    // rutas
+  val userCSV = "data/raw/googleplaystore_user_review.csv"
+  val playStoreCSV = "data/raw/googleplaystore.csv"
+
+  val spark = SparkSession
+    .builder()
+    .appName("TransformarCSV")
+    .master("local[*]")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val dfUsers = spark.read.option("header", "true").csv(userCSV)
+  val dfPlayStore = spark.read.option("header", "true").csv(playStoreCSV)
+
+    
+
+    val ejercicio = "4" 
+
+    ejercicio match {                                                                   
+      case "1"   => EjerciciosDataFrame.ejercicio1(dfPlayStore)
+      case "2"   => EjerciciosDataFrame.ejercicio2(dfPlayStore)
+      case "3"   => EjerciciosDataFrame.ejercicio3(dfPlayStore)
+      case "4"   => EjerciciosDataFrame.ejercicio4(dfPlayStore)
+      case "5"   => EjerciciosDataFrame.ejercicio5(dfPlayStore)
+      case "6a"  => EjerciciosDataFrame.ejercicio6a(dfPlayStore)
+      case "6b"  => EjerciciosDataFrame.ejercicio6b(dfPlayStore)
+      case "6c"  => EjerciciosDataFrame.ejercicio6c(dfPlayStore)
+      case "7"   => EjerciciosDataFrame.ejercicio7(dfPlayStore)
+      case _     => println(s"Ejercicio '$ejercicio' no reconocido.")
+    }
+  }
 }
